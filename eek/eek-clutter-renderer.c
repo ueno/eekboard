f094466d640d7a3a3d2bf1344a8b05961e6388b3
Daiki Ueno <ueno@unixuser.org>
2011-01-25 11:10:53 +0900

    Add license text.

--- eek/eek-clutter-renderer.c
+++ eek/eek-clutter-renderer.c
@@ -1,8 +1,32 @@
+/* 
+ * Copyright (C) 2010 Daiki Ueno <ueno@unixuser.org>
+ * Copyright (C) 2010 Red Hat, Inc.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
 #include <string.h>
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include <cogl/cogl.h>
 #include <cogl/cogl-pango.h>
 #include <clutter/clutter.h>
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif  /* HAVE_CONFIG_H */
 
 #include "eek-clutter-renderer.h"
 #include "eek-key.h"
